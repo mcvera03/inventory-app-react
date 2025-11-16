95b262e04de5c9e07e4e00e379f325e612348152
Carmen Vera <92607932+mcvera03@users.noreply.github.com>
2025-11-16 00:07:13 -0600

    Add files via upload

---
+++ src/pages/Ayuda.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Ayuda(){
+  return (
+    <div>
+      <h1>Ayuda y Soporte</h1>
+      <ul>
+        <li>Comenzar rapido</li>
+        <li>Centro de ayuda</li>
+        <li>Contactar soporte</li>
+        <li>Tutoriales</li>
+        <li>Novedades</li>
+        <li>Estado del sistema</li>
+      </ul>
+    </div>
+  )
+}
---
+++ src/pages/Usuario.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Usuario(){
+  return (
+    <div>
+      <h1>Usuario</h1>
+      <p>Perfil y configuración de usuario (próximo)</p>
+    </div>
+  )
+}
