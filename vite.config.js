354699cfef64ef3607566e1e7fbaffb241ce014e
Carmen Vera <92607932+mcvera03@users.noreply.github.com>
2025-11-18 01:52:05 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: 'public',
   plugins: [react()],
-  server: {
-    port: 5173
-  },
   build: {
-    outDir: 'dist'
+    outDir: '../dist',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 })
