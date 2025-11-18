ad49afc401680540c77b63646dfd6faa4e0680aa
Carmen Vera <92607932+mcvera03@users.noreply.github.com>
2025-11-18 02:18:36 -0600

    Remove server port and rollup input configuration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'public/index.html')
-      }
-    },
     outDir: 'dist',
     emptyOutDir: true
   }
