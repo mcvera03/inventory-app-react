9abf847896350bb35ab29831cae4371b8a64090f
Carmen Vera <92607932+mcvera03@users.noreply.github.com>
2025-11-19 00:14:04 -0600

    Delete src/pages/Reports.jsx

--- src/pages/Reports.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default function Reports(){
-  return (
-    <div className="container">
-      <h2>Reportes</h2>
-      <p>Funcionalidad de reportes no implementada (placeholder).</p>
-    </div>
-  )
-}
